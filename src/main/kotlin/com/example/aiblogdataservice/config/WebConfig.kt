b12f64b5995a3f7557c7b497a7660fc2f5fc6485
Alex <alexplaza0495@gmail.com>
2023-07-14 20:33:01 -0700

    cors

---
+++ src/main/kotlin/com/example/aiblogdataservice/config/WebConfig.kt
@@ -0,0 +1,14 @@
+package com.example.aiblogdataservice.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://ai-blog-data-service-2902779ebc41.herokuapp.com")
+                .allowedMethods("*")
+    }
+}
