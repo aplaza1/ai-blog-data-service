fb659557152b4727eec95e36d2b6ac5aa6431f37
Alex <alexplaza0495@gmail.com>
2023-07-15 23:46:24 -0700

    add fqdn

--- src/main/kotlin/com/example/aiblogdataservice/config/WebConfig.kt
+++ src/main/kotlin/com/example/aiblogdataservice/config/WebConfig.kt
@@ -12,7 +12,7 @@ class WebConfig : WebMvcConfigurer {
                 .allowedOrigins("https://ai-blog-frontend-b47e4646b476.herokuapp.com")
                 .allowedOrigins("http://alexplaza.me")
                 .allowedOrigins("https://alexplaza.me")
-                .allowedOrigins("https://postscribbled.com")
+                .allowedOrigins("https://www.postscribbled.com")
                 .allowedMethods("*")
     }
 }
