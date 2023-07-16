8bc60aa0f34e655178037ab19b027a7b2d501e7e
Alex <alexplaza0495@gmail.com>
2023-07-15 17:31:18 -0700

    fix cors

--- src/main/kotlin/com/example/aiblogdataservice/web/AIBlogController.kt
+++ src/main/kotlin/com/example/aiblogdataservice/web/AIBlogController.kt
@@ -16,7 +16,7 @@ class AIBlogController (
         return aiBlogService.saveBlogPost(blogPost)
     }
 
-    @CrossOrigin("https://ai-blog-data-service-2902779ebc41.herokuapp.com")
+    @CrossOrigin(origins = ["https://ai-blog-data-service-2902779ebc41.herokuapp.com"])
     @GetMapping("/blog-posts")
     fun getBlogPost(@RequestParam title: String): BlogPost? {
             return aiBlogService.getBlogPost(title)
